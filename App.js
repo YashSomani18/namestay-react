dcf2f2cfb487adf1331aea67d53fdd0ab0a9b74f
YashSomani18 <yashsomani18@gmail.com>
2023-08-30 19:55:56 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,16 @@
+const heading = React.createElement("h1", {id: "heading"}, "Hello by React js");
+
+const parent = React.createElement("div",{id:"div"},
+React.createElement("h1", {id: "heading"}, "Hello by React js"));
+
+const second = React.createElement("div",{id: "division"}, [
+    React.createElement("h1",{id:"head1"},"Hello myself React"),
+    React.createElement("h2",{id:"head2"},"Hello i am second react")
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(second); //object
+
+root.render(second);
+
+
