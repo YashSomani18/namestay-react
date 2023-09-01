68c190268c2783ac41518dfd25f8a41cc6fd299c
YashSomani18 <yashsomani18@gmail.com>
2023-09-02 03:30:54 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,19 +1,37 @@
 import React from "react";
-import ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom";
 
-const heading = React.createElement("h1", {id: "heading"}, "Hello by React js");
+const heading = React.createElement("h1",{id:"heading"},"Nameste React Object");
+console.log(heading);
 
-const parent = React.createElement("div",{id:"div"},
-React.createElement("h1", {id: "heading"}, "Hello by React js"));
+const jsxHeading = <h1 id="heading"> This is React using JSX</h1>;
+console.log(jsxHeading);
 
-const second = React.createElement("div",{id: "division"}, [
-    React.createElement("h1",{id:"head1"},"Hello myself React"),
-    React.createElement("h2",{id:"head2"},"Hello i am second react")
-]);
 
+const Title = () =>(
+  <h2>This is cooler React functional component</h2>  
+);
+
+const HeadingComponent = () =>{
+    return (
+        <div>
+            <Title/>
+        <h1 className="heading">This is react functional component</h1>
+
+        </div>
+    )
+}
+
+const Func = () => {
+    return (
+        <div>
+            {HeadingComponent()}
+            <HeadingComponent></HeadingComponent>
+            <HeadingComponent/>
+            <p>Do you know how yash is such a hardworking kid</p>
+        </div>
+    );
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(second); //object
-
-root.render(second);
-
-
+// root.render(jsxHeading); // this is how you render your react element
+root.render(<Func/>); // this is how you render your react component
