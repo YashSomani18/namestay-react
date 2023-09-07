468d3927a42d4cbec017dc6c70baf99dc892acd4
YashSomani18 <yashsomani18@gmail.com>
2023-09-08 00:03:05 +0530

    episode-07

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <div>
+      <h1> About</h1>
+      <h2> THis is learning by Yash SOmani</h2>
+    </div>
+  );
+};
+export default About;
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
 import Shimmer from "./Shimmer";
+import { Link } from "react-router-dom";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
@@ -85,7 +86,12 @@ const Body = () => {
       </div>
       <div className="res-container">
         {filterRestaurant.map((restaurant) => (
-          <RestaurantCard key={restaurant.info.id} resData={restaurant.info} />
+          <Link
+            key={restaurant.info.id}
+            to={"/restaurants/" + restaurant.info.id}
+          >
+            <RestaurantCard resData={restaurant.info} />
+          </Link>
         ))}
       </div>
     </div>
